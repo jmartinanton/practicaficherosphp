edec8a1c9a63557b6533ff5d95af67be9e83bda9
jmart <jmart@DESKTOP-4TH3JSB.EQT.local>
2016-04-21 16:42:28 +0200

    dfg

--- modelo/clases/directorio.php
+++ modelo/clases/directorio.php
@@ -5,8 +5,27 @@ class Directorio {
 	private $directorio;
 	private $directorioAbierto;
 	
-	function _construct(){
+	function _construct($rutaDirectorio){
+		$this->directorio = $rutaDirectorio;
+	}
+	
+	function abrir(){
+		$this->directorioAbierto = opendir($this->directorio);
+	}
+	
+	function cerrar(){
+		closedir($this->directorioAbierto);
+	}
+	
+	function getContenido(){
+		while ($contenido = readdir($this->directorioAbierto)){
+			if (is_dir($contenido)){
+				echo "<lh>" . $contenido . "</lh>";
+			} else echo "<li>" . $contenido . "</lh>";
+		}
 		
 	}
+	
+	
 	
 }
