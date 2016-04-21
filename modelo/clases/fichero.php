8b2bbaec5f47b19a3e1d25dd01bc2b60174d0540
jmart <jmart@DESKTOP-4TH3JSB.EQT.local>
2016-04-21 16:12:06 +0200

    añadido fichero.php

---
+++ modelo/clases/fichero.php
@@ -0,0 +1,42 @@
+<?php
+
+class Fichero {
+	
+	private $fichero;
+	private $ficheroAbierto;
+	
+	function _construct(){
+		
+	}
+	
+	function abre(){
+		
+	}
+	
+	function cierra(){
+		
+	}
+	
+	function leeEntero(){
+		
+	}
+	
+	function leeLinea(){
+		
+	}
+	
+	function anadirAlFinal(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
