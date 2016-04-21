5caf78fc55dc38fac78b9d330af06533dcfd2d59
Marc <Marc@Marc-PC>
2016-04-21 16:10:14 +0200

    y

--- menu.php
+++ menu.php
@@ -2,7 +2,19 @@
 <body>
 
 <form action="mostrarlinies.php" method="post">
-<input type="submit" value="mostrar linias"/>
+<input type="submit" value="mostrar lineas"/>
+</form>
+<form action="mostrarContingutTotal.php" method="post">
+<input type="submit" value="mostrar todo el contenido"/>
+</form>
+<form action="mostrarNouContingut.php" method="post">
+<input type="submit" value="añadir contenido al fichero"/>
+</form>
+<form action="neteja.php" method="post">
+<input type="submit" value="netejar"/>
+</form>
+<form action="eliminar.php" method="post">
+<input type="submit" value="eliminar"/>
 </form>
 
 </body>
